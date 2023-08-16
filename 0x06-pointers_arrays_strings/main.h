31f684fed31a9f51d5e8b18a1b24014bf297ce95
MondeAnna <monde.anna@gmail.com>
2023-08-16 08:24:46 +0200

    Pointers 2 (Task 03): Compare Two Strings
        - Course Instructions:
            - Write a function that compares two strings
    
        - Prototype:
            - `int _strcmp(char *s1, char *s2);`
    
        - Spec:
            - Your function should work exactly like strcmp
    
         - FYI:
             - The standard library provides a similar
               function: `strcmp`
             - Run `man strcmp` to learn more

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,11 +5,14 @@
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 int _strlen(char *s);
 void test_strcat();
 void test_strncat_len_greater_than_src(void);
 void test_strncat_len_less_than_src(void);
+void test_strcmp_equal_char_len(void);
+void test_strcmp_variadic_char_len(void);
 void test_strncpy_len_equal_to_src(void);
 void test_strncpy_len_greater_than_src(void);
 void test_strncpy_len_less_than_src(void);
