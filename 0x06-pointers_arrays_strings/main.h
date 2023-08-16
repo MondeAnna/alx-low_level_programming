645dddc4299bc504f76d9fe7e1589da109770b83
MondeAnna <monde.anna@gmail.com>
2023-08-16 08:20:48 +0200

    Pointers 2 (Task 01): Append `n` `chars` to a String
        - Course Instructions:
            - Write a function that concatenates two strings
    
        - Prototype:
            - `char *_strncat(char *dest, char *src, int n);`
    
        - Return:
            - a pointer to the resulting string `dest`
    
        - Spec:
            - The `_strncat` function is similar to the
              `_strcat` function, except that it will use
              at most `n` bytes from `src`; and
            - `src` does not need to be null-terminated if
               it contains `n` or more bytes
    
        - FYI:
            - The standard library provides a similar function:
              `strncat`
            - Run `man strncat` to learn more

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,7 +4,10 @@
 #include <assert.h>
 
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 int _strlen(char *s);
 void test_strcat();
+void test_strncat_len_greater_than_src(void);
+void test_strncat_len_less_than_src(void);
 
 #endif /* MAIN_H */
