75241428ca4fdf3723cfba41a3d80e4b99de2c34
MondeAnna <monde.anna@gmail.com>
2023-08-23 10:43:25 +0200

    Recursion (Task 05): Determin Natural Square-Root
        - Course Instructions:
            - Write a function that returns the natural square
              root of a number
    
        - Limitations:
            - Prototype:
                - `int _sqrt_recursion(int n);`
            - If `n` does not have a natural square root, the
              function should return `-1`
    
        - FYI:
            - The standard library provides a different
              function: `sqrt`
            - Run `man sqrt` to learn more.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,8 @@ int _pow_recursion(int x, int y);
 void _print_rev_recursion(char *s);
 int _putchar(char c);
 void _puts_recursion(char *s);
+int _sqrt_iterator(int num, int div);
+int _sqrt_recursion(int n);
 int _strlen_recursion(char *s);
 int factorial(int n);
 
