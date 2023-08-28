951ef14ef4585cb2ce3ee4a25a36fa7990d5af5f
MondeAnna <monde.anna@gmail.com>
2023-08-28 10:43:25 +0200

    Malloc (Task 03): Dynamically Allocate Memory to 2-D Array
        - Course Instructions:
            - Write a function that returns a pointer to a 2
              dimensional array of integers
    
        - Limitations:
            - Prototype:
                - `int **alloc_grid(int width, int height);`
            - Initialise all elements to `0`
            - Return `NULL` on failure
            - Where `width` or `height` are not greater than `0`,
              return `NULL`

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,9 +3,10 @@
 
 #include <stdlib.h>
 
-char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 char *_strdup(char *str);
 int _strlen(const char *str);
 char *create_array(unsigned int size, char c);
+char *str_concat(char *s1, char *s2);
 
 #endif /* MAIN_H */
