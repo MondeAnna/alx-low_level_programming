2f30e832a2e2ecd359b3a71606bafc0237abbafe
manna023 <manna023@student.wethinkcode.co.za>
2023-09-11 10:43:25 +0200

    Printf (Task 00): Handle Variadic String Input
        - accidental case of buffering by determining size
          of string prior to calling `write`
    
        - Course Instructions:
            - Write a function that produces output
              according to a format
    
        - Limitations:
            - Prototype:
                - `int _printf(const char *format, ...);`
            - Returns:
                - number of characters printed, bar null byte
            - Write output to `stdout`
            - `format` is a character string composed of zero
              or more directives (see `man 3 printf` for more details
            - Handle the conversion specifiers
                - `c`
                - `s`
                - `%`
            - no need to handle
                - reproducing buffer handling of c-lib `printf`
                - flag characters
                - filed width
                - precision
                - length modifiers

---
+++ 0x11-printf/tests/main_test.c
@@ -0,0 +1,18 @@
+#include "main_test.h"
+
+/**
+ * main - entry point
+ *
+ * Return: EXIT_SUCCESS
+ */
+int main(void)
+{
+	no_va_char_test();
+	no_va_str_test();
+
+	spec_percent_test();
+	spec_c_test();
+	spec_s_test();
+
+	return (EXIT_SUCCESS);
+}
