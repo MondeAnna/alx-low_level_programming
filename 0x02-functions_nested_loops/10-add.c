9db527a44a8741bd7bf758cd8b39aa9548c50b84
MondeAnna <monde.anna@gmail.com>
2023-10-10 20:49:44 +0200

    Functions (Task 10): Algebra -> Add Two Integers
        - Course Instructions:
            - Write a function that adds two integers and returns the result.
    
        - Limitations:
             - Prototype: `int add(int, int);`

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Add two integers
+ * @a: left operand
+ * @b: right operand
+ *
+ * Return: sum of operands (int)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
