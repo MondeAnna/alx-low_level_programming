5570b0221e4e088b7bb9c042cc2ad2fce1da6eed
MondeAnna <monde.anna@gmail.com>
2023-10-10 20:46:28 +0200

    Functions (Task 08): Show 24 Hour Military-Styled Clock
        - Course Instructions:
             - Write a function that prints every minute of
               the day of Jack Bauer, starting from 00:00 to
               23:59
    
        - Limitations:
            - Prototype: `void jack_bauer(void);`

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,7 @@ int _abs(int);
 int _isalpha(int);
 int _islower(int);
 int _putchar(char);
+void jack_bauer(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int print_last_digit(int);
