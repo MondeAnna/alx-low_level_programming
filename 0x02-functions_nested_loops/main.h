ebdd0e5b071119cf7409a2b8bf763c75d3063bed
MondeAnna <monde.anna@gmail.com>
2023-10-10 19:56:51 +0200

    Functions (Task 04): Bool -> Determine if `char` is an alphabet
        - Course Instructions:
            - Write a function that checks for alphabetic character.
    
        - Limitations:
            - Prototype: `int _isalpha(int c);`
            ` Returns `1` if c is a letter, lowercase or uppercase,
              `0` otherwise
    
        - FYI:
            - The standard library provides a similar function:
              `isalpha`. Run `man isalpha` to learn more

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,10 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+int _isalpha(int c);
+int _islower(int c);
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
 
 #endif /* _MAIN_H */
