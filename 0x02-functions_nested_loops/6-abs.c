dfdb4cc1b9833e21f0561dc964c1d6557dbdea9a
MondeAnna <monde.anna@gmail.com>
2023-10-10 20:34:05 +0200

    Functions (Task 06): Algebra -> Determine Absolute Value
        - Course Instructions:
            - Write a function that computes the absolute value of an integer.
    
        - Limitations:
            - Prototype: `int _abs(int);`
    
        - FYI:
            - The standard library provides a similar function:
              `abs`. Run `man abs` to learn more

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - Determine absolute value
+ * @n: Number being determined
+ *
+ * Return: Absolute value of n (int)
+ */
+int _abs(int n)
+{
+	return (n >= 0 ? n : -n);
+}
