a8c0ff26a9ccb2dc43ebfa9b30784a1e8ce3c546
MondeAnna <monde.anna@gmail.com>
2023-10-16 18:58:30 +0200

    Pointers (Task 00): Reset `int` to `98`
        - Course Instructions:
            - Write a function that takes a pointer to an
              `int` as parameter and updates the value it
              points to to `98`
    
        - Prototype:
            - `void reset_to_98(int *n);`

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset int to 98
+ * @n: pointer to int being changed
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -37,4 +37,6 @@ The ability to explain, without external resources:
 
 #### Low Level Programming Projects
 
+* _[`Reset int to 98`](0-reset_to_98.c)_
+
 <br />
