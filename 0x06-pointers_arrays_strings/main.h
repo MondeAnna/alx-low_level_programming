ddceca4c1d972c66ce99c4364b21456c4f14c657
MondeAnna <monde.anna@gmail.com>
2023-10-18 20:34:27 +0200

    Pointers 2 (Task 02): Copy `n` `chars` from a String to Another
        - Course Instructions:
            - Write a function that copies a string
    
        - Prototype:
            - char *_strncpy(char *dest, char *src, int n);`
    
        - Spec:
            - Your function should work exactly like `strncpy`
    
        - FYI:
            - The standard library provides a similar function:
              `strncpy`
            - Run man `strncpy` to learn more

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,9 +5,13 @@
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strlen(char *s);
 void test_strcat();
 void test_strncat_len_greater_than_src(void);
 void test_strncat_len_less_than_src(void);
+void test_strncpy_len_equal_to_src(void);
+void test_strncpy_len_greater_than_src(void);
+void test_strncpy_len_less_than_src(void);
 
 #endif /* MAIN_H */
