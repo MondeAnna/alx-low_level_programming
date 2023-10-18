5f33abd7ed1597a664dbdc085b9ce60a30b1cd8d
MondeAnna <monde.anna@gmail.com>
2023-10-18 22:54:35 +0200

    Pointers 2 (Task 04): Reverse Values in Array
        - Course Instructions:
             - Write a function that reverses the content of
               an array of integers
    
        - Prototype:
            - `void reverse_array(int *a, int n);`
    
        - Spec:
            - Where `n` is the number of elements of the
              array

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,6 +8,8 @@ char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 int _strlen(char *s);
+void reverse_array(int *a, int n);
+void swap(int *a, int *b);
 void test_strcat();
 void test_strncat_len_greater_than_src(void);
 void test_strncat_len_less_than_src(void);
