4d34ca29abf037e3bcee34bb25325da0af61a2d0
MondeAnna <monde.anna@gmail.com>
2023-10-19 18:34:38 +0200

    Pointers 2 (Task 06): Capitalise All Words in a String
        - Course Instructions:
            - Write a function that capitalizes all words of
              a string
    
        - Prototype:
            - `char *cap_string(char *);`
    
        - Word Separators
            - space, tabulation, new line,
              `,`, `;`, `.`, `!`, `?`, `"`, `(`, `)`, `{`,
              and `}`

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,14 +3,19 @@
 
 #include <assert.h>
 
+int _islower(int c);
+int _isseparator(const char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 int _strlen(char *s);
+char *cap_string(char *str);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 void swap(int *a, int *b);
+void test_cap_string_special_char(void);
+void test_cap_string_whitespace(void);
 void test_strcat();
 void test_strncat_len_greater_than_src(void);
 void test_strncat_len_less_than_src(void);
