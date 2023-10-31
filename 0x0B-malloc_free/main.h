2ba3f0f2938b514d304c40e9becaa1555549a488
MondeAnna <monde.anna@gmail.com>
2023-10-31 09:46:23 +0200

    Malloc (Task 05): Concatenate Multiple Strings into One
        - Course Instructions:
            - Write a function that concatenates all the
              arguments of your program
    
        - Limitations:
            - Prototype:
                - `char *argstostr(int ac, char **av);`
            - Returns `NULL` if `ac == 0` or `av == NULL`
            - Returns a pointer to a new string, or `NULL`
              if it fails
            - Each argument should be followed by a `\n`
              in the new string

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,11 +3,16 @@
 
 #include <stdlib.h>
 
-int **alloc_grid(int width, int height);
+int _get_n_words(char *str);
+char *_memcpy(char *dest, char *src, int n);
+int _putchar(char c);
+char *_strcat(char *s1, char *s2);
 char *_strdup(char *str);
 int _strlen(const char *str);
+int **alloc_grid(int width, int height);
+char *argstostr(int ac, char **av);
 char *create_array(unsigned int size, char c);
 void free_grid(int **grid, int height);
-char *str_concat(char *s1, char *s2);
+char **strtow(char *str);
 
 #endif /* MAIN_H */
