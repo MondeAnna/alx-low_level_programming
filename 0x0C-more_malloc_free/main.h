354994691605381cf723191c3b48aabe4d735057
MondeAnna <monde.anna@gmail.com>
2023-11-02 19:50:40 +0200

    Malloc 2 (Task 01): Concatenate Two Strings
        - Course Instructions:
            - Write a function that concatenates two strings
    
        - Limitations:
            - Prototype:
                - `char *string_nconcat(
                      char *s1,
                      char *s2,
                      unsigned int n
                  );`
            - The returned pointer shall point to a newly
              allocated space in memory, which contains `s1`,
              followed by the first `n` bytes of `s2`, and
              null terminated
            - If the function fails, it should return `NULL`
            - If `n` is greater or equal to the length of `s2`
              then use the entire string `s2`
            - If `NULL` is passed, treat it as an empty
              string

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 
+unsigned int _strlen(const char *str);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif /* MAIN_H  */
