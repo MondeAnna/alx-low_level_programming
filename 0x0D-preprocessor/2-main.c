2bb3f5927a34cfa495495a4aeee41841d1a663e3
manna023 <manna023@student.wethinkcode.co.za>
2023-11-06 14:42:12 +0200

    Pre-Proc (Task 02): Display Compilation File Name
        - Course Instructions:
            - Write a program that prints the name of the
              file it was  compiled from, followed by a
              new line
    
        - Limitations:
            - Use of the standard library is allowed

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: display compilation file name
+ *
+ * Return: EXIT_SUCCESS
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (EXIT_SUCCESS);
+}
