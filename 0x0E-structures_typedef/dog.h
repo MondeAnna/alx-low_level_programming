616133bd8c5af5a8c7bfc661e2b524d07909175f
manna023 <manna023@student.wethinkcode.co.za>
2023-11-16 14:09:40 +0200

    Struct (Task 00): Dog Object
        - Course Instructions:
            - Define a new type `struct dog`
    
        - Limitations:
            - Type elements:
                - `name`, of type `char *`
                - `age`, of type `float`
                - `owner`, of type `char *`

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog object
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog's owner
+ *
+ * Description: dog object
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
