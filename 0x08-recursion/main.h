c9f0806cdfdc4ee6f1ad07a3dafa46dbdf8b97f6
MondeAnna <monde.anna@gmail.com>
2023-11-19 17:22:44 +0200

    Recursion (Task 07): Determine if `str` is a Palindrome
        - Course Instructions:
            - Write a function that returns `1` if a string is
              a palindrome and `0` if not
    
        - Limitations:
            - Prototype:
                - `int is_palindrome(char *s);`
            - An empty string is a palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,8 @@
 #define FALSE 0
 #define TRUE 1
 
+char *_get_last_char(char *s);
+int _iterator(char *begin, char *end);
 int _iterator_n(int num, int mod);
 int _pow_recursion(int x, int y);
 void _print_rev_recursion(char *s);
@@ -16,5 +18,6 @@ int _sqrt_recursion(int n);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
 
 #endif /* MAIN_H */
