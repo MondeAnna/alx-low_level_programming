69f6caf1061b71aa16edfe2aa762f1c46d5d0fe5
MondeAnna <monde.anna@gmail.com>
2023-11-23 09:18:42 +0200

    Bit Manip (Task 02): Get Bit at Given Index
        - Course Instructions:
            - Write a function that returns the value of a bit
              at a given index
    
        - Prototype:
            - `int get_bit(unsigned long int n, unsigned int index);`
    
        - Returns:
            - bit at index `index` or `-1` if an error occured
    
        - Spec:
            - `index` is the index, starting from `0` of the
               bit you want to get

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,10 +2,12 @@
 #define MAIN_H
 
 int _is_binary(const char *b);
+int _is_odd(const long int n);
 unsigned int _pow(const int b, int i);
 int _putchar(char c);
 int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
+int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 
 #endif /* MAIN_H */
