d56ec99dc34de72319f984e54aaeee7bc3f701c4
MondeAnna <monde.anna@gmail.com>
2023-11-23 09:14:56 +0200

    Bin Manip (Task 01): Print Binary Form of `int`
        - Course Instructions:
            - Write a function that prints the binary
              representation of a number
    
        - Prototype:
            - `void print_binary(unsigned long int n);`
    
        - Spec:
            - You are not allowed to use arrays
            - You are not allowed to use `malloc`
            - You are not allowed to use the `%` or `/`
              operators

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,9 @@
 
 int _is_binary(const char *b);
 unsigned int _pow(const int b, int i);
+int _putchar(char c);
 int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif /* MAIN_H */
