369be946a034eaab0f5154e9ae5cd52140b260e3
MondeAnna <monde.anna@gmail.com>
2024-01-15 10:43:26 +0200

    Dynamic Lib (Task 02): Basic Math Operations
        - Course Instructions:
            - Create a dynamic library that contains `C`
              functions that can be called from `Python`
    
        - Example:
    
        ```
        julien@ubuntu:~/0x18$ cat 100-tests.py
        import random
        import ctypes
    
        cops = ctypes.CDLL('./100-operations.so')
        a = random.randint(-111, 111)
        b = random.randint(-111, 111)
        print("{} + {} = {}".format(a, b, cops.add(a, b)))
        print("{} - {} = {}".format(a, b, cops.sub(a, b)))
        print("{} x {} = {}".format(a, b, cops.mul(a, b)))
        print("{} / {} = {}".format(a, b, cops.div(a, b)))
        print("{} % {} = {}".format(a, b, cops.mod(a, b)))
        julien@ubuntu:~/0x16. Doubly linked lists$ python3 100-tests.py
        66 + -76 = -10
        66 - -76 = 142
        66 x -76 = -5016
        66 / -76 = 0
        66 % -76 = 66
        julien@ubuntu:~/0x18$ python3 100-tests.py
        ```

--- 0x18-dynamic_libraries/README.md
+++ 0x18-dynamic_libraries/README.md
@@ -37,5 +37,6 @@ The ability to explain, without external resources:
 
 * _[`Create Dynamic Library`](libdynamic.so)_
 * _[`Create Dynamic List via Script`](1-create_dynamic_lib.sh)_
+* _[`Basic Math Operations`](100-operations.so)_
 
 <br />
---
+++ 0x18-dynamic_libraries/operations/maths.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * add - add
+ * @a: left operand
+ * @b: left operand
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub
+ * @a: left operand
+ * @b: left operand
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul
+ * @a: left operand
+ * @b: left operand
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div
+ * @a: left operand
+ * @b: left operand
+ * Return: int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod
+ * @a: left operand
+ * @b: left operand
+ * Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
